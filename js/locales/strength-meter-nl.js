4df758bb7417af356f77cbac374bfaa87951b81c
lars-t <lars-t@users.noreply.github.com>
2014-05-06 22:11:31 +0200

    Dutch translation

---
+++ js/locales/strength-meter-nl.js
@@ -0,0 +1,20 @@
+/**
+ * Strength Meter Dutch translation.
+ * This file must be saved in UTF-8 encoding.
+ * @copyright &copy; Kartik Visweswaran, Krajee.com, 2013
+ */
+(function ($) {
+    "use strict";
+
+    $.extend($.fn.strength.defaults, {
+        toggleTitle: 'Wachtwoord tonen / verbergen',
+        verdictTitles: {
+            0: 'te kort',
+            1: 'heel zwak',
+            2: 'zwak',
+            3: 'goed',
+            4: 'sterk',
+            5: 'heel sterk'
+        }
+    });
+})(jQuery);
