2cc6b4d0e86d73246edd9c79a52b19112eb5e122
Tibor Balogh <tibee@tibee.hu>
2015-11-03 11:51:53 +0100

    Create strength-meter-hu.js
    Hungarian translation.

---
+++ js/locales/strength-meter-hu.js
@@ -0,0 +1,20 @@
+/**
+ * Strength Meter Hungarian translation.
+ * This file must be saved in UTF-8 encoding.
+ * @copyright &copy; Kartik Visweswaran, Krajee.com, 2015
+ */
+(function ($) {
+    "use strict";
+
+    $.fn.strengthLocales['hu'] = {
+        toggleTitle: 'Jelszó mutatása / elrejtése',
+        verdictTitles: {
+            0: 'túl rövid',
+            1: 'gyenge',
+            2: 'közepes',
+            3: 'jó',
+            4: 'erős',
+            5: 'nagyon erős'
+        }
+    };
+})(jQuery);
