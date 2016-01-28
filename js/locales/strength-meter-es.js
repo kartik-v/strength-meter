9e9b8b53168bb3e18c63f481e4e3b6564e088adb
Luis Chanferoni <luichanfe@gmail.com>
2016-01-28 20:20:24 -0300

    Create new file strength-meter-es.js
    Spanish translation

---
+++ js/locales/strength-meter-es.js
@@ -0,0 +1,20 @@
+/**
+ * Strength Meter Spanish translation.
+ * This file must be saved in UTF-8 encoding.
+ * @copyright &copy; Kartik Visweswaran, Krajee.com, 2013
+ */
+(function ($) {
+    "use strict";
+
+    $.extend($.fn.strength.defaults, {
+        toggleTitle: 'Mostrar / Ocultar contraseña',
+        verdictTitles: {
+            0: 'Muy corta',
+            1: 'Muy débil',
+            2: 'Débil',
+            3: 'Buena',
+            4: 'Fuerte',
+            5: 'Muy Fuerte'
+        }
+    });
+})(jQuery);
